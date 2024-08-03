b539e044daca6051a7f6b255caed407e88e16daa
dustytrash <Lionelbergen@live.com>
2024-08-03 15:47:04 -0500

    Working bluetooth sensor

--- morse-code-sensor.ino
+++ morse-code-sensor.ino
@@ -1,20 +1,24 @@
-// LED:
-// A = Positive
-// C = Negative
+#include<SoftwareSerial.h>
 
-const int OUTPUT_LED_PIN = 12;
-const int INPUT_BUTTON_PIN = 2;
+const int RX_PIN = 12;
+const int TX_PIN = 2;
+const int INPUT_BUTTON_PIN = 3;
+
+const int BAUD_RATE = 9600;
+
+const SoftwareSerial bluetooth = SoftwareSerial(RX_PIN, TX_PIN);
 
 void setup() {
+  // Activate the internal pull-up resistor
   pinMode(INPUT_BUTTON_PIN, INPUT_PULLUP);
-  pinMode(OUTPUT_LED_PIN, OUTPUT);
+   
+  bluetooth.begin(9600);
 }
 
 void loop() {
   // If button pushed
   if (digitalRead(INPUT_BUTTON_PIN) == LOW) {
-    digitalWrite(OUTPUT_LED_PIN, HIGH);
+    bluetooth.print("x\n");
   } else {
-    digitalWrite(OUTPUT_LED_PIN, LOW);
   }
 }
